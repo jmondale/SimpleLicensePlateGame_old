7ab8fa8b0e530bc58d535a60b0f9d7ec3c7131b0
Jaye Mondale <jmondale@gmail.com>
2024-07-18 08:25:29 -0600

    Remove colors for now

--- SimpleLicensePlateGame/SimpleLicensePlateGame/ContentView.swift
+++ SimpleLicensePlateGame/SimpleLicensePlateGame/ContentView.swift
@@ -19,25 +19,25 @@ struct ContentView: View {
                         Text("States Score")
                             .font(.headline)
                             .padding(.leading)
-                            .foregroundColor(Color.appPrimary)
+                            //.foregroundColor(Color.appPrimary)
                         ScoreView(total: viewModel.totalStates, found: viewModel.foundStates)
                             .padding(.bottom, 10)
                     }
                     
                 }
-                .background(Color.appBackground)
+                //.background(Color.appBackground)
                 
                 // States Section
                 Section(header: Text("States")
-                    .foregroundColor(Color.appPrimary)
+                    //.foregroundColor(Color.appPrimary)
                     .padding()
-                    .background(Color.appBackground)
+                    //.background(Color.appBackground)
                 ) {
                     ForEach(viewModel.states) { state in
                         StateRow(viewModel: viewModel, state: state)
                         
                     }
-                    .listRowBackground(Color.appBackground.opacity(0.2))
+                    //.listRowBackground(Color.appBackground.opacity(0.2))
                 }
                 
                 // Province Score Section
@@ -46,26 +46,27 @@ struct ContentView: View {
                         Text("Provinces Score")
                             .font(.headline)
                             .padding(.leading)
-                            .foregroundColor(.primary)
+                            //.foregroundColor(.primary)
                         ScoreView(total: viewModel.totalProvinces, found: viewModel.foundProvinces)
                     }
                     .padding(.bottom, 10)
                 }
-                .background(Color.background)
+                //.background(Color.background)
                 
                 // Provinces Section
                 Section(header: Text("Provinces")
-                    .foregroundColor(Color.appPrimary)
+                    //.foregroundColor(Color.appPrimary)
                     .padding()
-                    .background(Color.appBackground)) {
+                    //.background(Color.appBackground)
+                ) {
                     ForEach(viewModel.provinces) { province in
                         ProvinceRow(viewModel: viewModel, province: province)
                     }
-                    .listRowBackground(Color.appBackground.opacity(0.2))
+                    //.listRowBackground(Color.appBackground.opacity(0.2))
                 }
             }
             .listStyle(PlainListStyle())
-            .background(Color.appBackground.edgesIgnoringSafeArea(.all))
+            //.background(Color.appBackground.edgesIgnoringSafeArea(.all))
             .navigationTitle("License Plates")
             .sheet(item: $viewModel.selectedState) { state in
                 StateDetailView(state: state)
